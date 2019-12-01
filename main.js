fcc13e480cb4953fc370e671176a29a181222e89
Angel Negron <negron.a@ufl.edu>
2019-12-01 13:13:57 -0500

    Javascript file created and code added

---
+++ main.js
@@ -0,0 +1,13 @@
+var members = [
+  "Dave Grohl",
+  "Nate Mendel",
+  "Pat Smear",
+  "Taylor Hawkins",
+  "Rami Jaffee",
+  "Chris Shiflett"
+];
+
+var tour =  {
+  city: ["Chicago", "Seattle", "Edmonton"];
+
+}
---
+++ scripts/main.js
@@ -0,0 +1,24 @@
+//Band Member names array
+var members = [
+  "Dave Grohl",
+  "Nate Mendel",
+  "Pat Smear",
+  "Taylor Hawkins",
+  "Rami Jaffee",
+  "Chris Shiflett"
+];
+
+//Tourd dates and cities
+var tour = {
+  chicago: "July 29th & 30th, 2018",
+  seattle: "September 1st, 2018",
+  edmonton:"September 4th, 2018"
+};
+
+//While loop
+var x = 0;
+while (x < 5) {
+  console.log("times running through the loop: "+ x);
+
+  x++;
+};
